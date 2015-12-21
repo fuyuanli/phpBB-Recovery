092e384299c29e530e3cd8f5978a35dd7b179f72
FuYuan Li <fuyuanli0625@gmail.com>
2015-12-22 00:20:27 +0800

    20151222

--- Passwd-Recovery/db.php
+++ Passwd-Recovery/db.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "127.6.161.2";
-$username = "phpbb";
-$password = "djxaQBCwanRYaJU3";
-$dbname = "phpbb";
+$servername = "127.0.0.1";	//資料庫位址
+$username = "phpbb";		//資料庫帳號
+$password = "";				//資料庫密碼
+$dbname = "phpbb";			//資料庫名稱
 ?>
